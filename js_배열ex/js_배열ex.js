364a0cf8c174ff8f0624bb6fd4545fe116c70bff
ljxxyxxg <leejy970103@gmail.com>
2024-07-02 17:31:49 +0900

    js_study

---
+++ js_배열ex/js_배열ex.js
@@ -0,0 +1,6 @@
+// 1.배열 요소를 뽑는방법
+let dataType = ['number', 'string', 'boolean', 'null', 'undefined', 'object'];
+
+for (let i = 0; i < dataType.length; i++) {
+    console.log(dataType[i]);
+}
---
+++ js배열/js_배열.js
@@ -0,0 +1,17 @@
+//배열(Array)
+let courseRanking = {
+    '1st': '자바스크립트 프로그래밍 기초',
+    '2nd': 'Git으로 배우는 버전 관리',
+    '3rd': '컴퓨터 개론',
+    '4th': '파이썬 프로그래밍 기초',
+};
+
+let Ranking = [ //중괄호 대신 대괄호
+    '자바스크립트 프로그래밍 기초',  //요소 element
+    'Git으로 배우는 버전 관리',
+    '컴퓨터 개론',
+    '파이썬 프로그래밍 기초',
+]; //대괄호 안에 index == PropertyName
+
+// indexing (0부터 시작)
+console.log(Ranking[1]);
