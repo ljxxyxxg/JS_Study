656acd9734f304cf91dd3de79d34cac23592155f
ljxxyxxg <leejy970103@gmail.com>
2024-07-04 14:05:43 +0900

    js_study

--- js배열/js_배열메소드.js
+++ js배열/js_배열메소드.js
@@ -3,5 +3,8 @@
 let members = ['쿠갈레', 'Zerrard66', '우리생각해써', '흙토끼', 'End Miracle'];
 
 console.log(members);
-delete members[1];
+
+
+//splice(시작인덱스, 삭제할 요소의 갯수, 추가할 요소1, 추가할 요소2, ...)
+members.splice(1, 1, 'NiceCodeit', 'HiCodeit');
 console.log(members);
